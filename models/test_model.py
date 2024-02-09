ca649228b4715efb1deb45688c57a71a24d72af7
obafisayo-alx <sekaniisjoy@gmail.com>
2024-02-09 21:48:40 +0100

    debugged pycodestyle for task 4

--- models/test_model.py
+++ models/test_model.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     #     print("\t{}: ({}) - {}".
     #           format(key, type(my_model_json[key]), my_model_json[key]))
 
-    #task 4
+    # task 4
     my_model = BaseModel()
     my_model.name = "My_First_Model"
     my_model.my_number = 89
@@ -28,7 +28,8 @@ if __name__ == "__main__":
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        print("\t{}: ({}) - {}".
+              format(key, type(my_model_json[key]), my_model_json[key]))
 
     print("------------")
     my_new_model = BaseModel(**my_model_json)
