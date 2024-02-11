691f7003c045070eea18093e1b2bc50c8446b954
obafisayo-alx <sekaniisjoy@gmail.com>
2024-02-11 19:54:48 +0100

    debugged console

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 
 import cmd
 import shlex
+import re
 from models.base_model import BaseModel
 from models.user import User
 from models.review import Review
@@ -17,8 +18,8 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB project"""
 
     prompt = "(hbnb) "
-    __valid_args = ["BaseModel",
-                    "User", "Review", "Amenity", "State", "Place", "City"]
+    __valid_args = {"BaseModel",
+                    "User", "Review", "Amenity", "State", "Place", "City"}
 
     def do_create(self, arg):
         """Creates a new instance of BaseModel"""
@@ -134,6 +135,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line"""
         pass
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
 
 if __name__ == '__main__':
